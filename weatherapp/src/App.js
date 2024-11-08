19c6118a2739d6471e50e0264d5389fafdda790b
Chbabula <mai2babulal786@gmail.com>
2024-11-08 14:32:37 +0530

    styles

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -24,6 +24,7 @@ function App() {
 
   return (
     <div className="app">
+      <h2 className='heading'>Hey Do You Want Weather Report Enter Your City Name</h2>
       <div className="search">
         <input
           value={location}
